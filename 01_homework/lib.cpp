2e6927e85c81f66fbf1d2c36aff8a96fd04fe61e
Ruslan <nugaev.ruslan2010@yandex.ru>
2024-02-05 16:56:37 +0300

    add files

---
+++ 01_homework/lib.cpp
@@ -0,0 +1,8 @@
+#include "lib.hpp"
+
+#include "version.h"
+
+int version()
+{
+    return PROJECT_VERSION_PATCH;
+}
---
+++ 01_homework/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "lib.h"
+
+int main()
+{
+    std::cout << "Project Version: " << version() << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
